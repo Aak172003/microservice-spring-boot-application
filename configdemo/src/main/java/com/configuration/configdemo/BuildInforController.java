7ae6a482e488ad4e78c88b3d0b1249e78ca05ae3
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-08-31 01:09:40 +0530

    active-profiledynamically-in-miroservice

--- configdemo/src/main/java/com/configuration/configdemo/BuildInforController.java
+++ configdemo/src/main/java/com/configuration/configdemo/BuildInforController.java
@@ -1,36 +1,59 @@
 package com.configuration.configdemo;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BuildInforController {
-//    Value annotation in spring boot is straightforward used to inject value to the variable from external sources such as application.properties or application.yml
 
-//    This is how we can set default kind of value
+//@RestController
+//public class BuildInforController {
+////    Value annotation in spring boot is straightforward used to inject value to the variable from external sources such as application.properties or application.yml
+//
+////-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//
+////    But configuration properties allow grouping of related properties to a single class.
+////    This makes application much more organized and easier to manage an application
+//
+//
+////    This is how we can set default kind of value
+////    @Value("${build.id:default}")
+////    This default value if we provide is it throws error like no active profile set.'
+//
+////    @Value("${build.id}")
+////    @Value("${OS:default}")
+////    If we provide default value and also we enable default profile active ,
+////    so this will show values from application.yml file because i activate default profile
 //    @Value("${build.id:default}")
-//    This default value if we provide is it throws error like no active profile set.'
+//    private String buildId;
+//
+////    @Value("${USERPROFILE:default}")
+//
+//    @Value("${build.version:default}")
+//    private String buildVersion;
+//
+////    @Value("${JAVA_HOME:default}")
+//    @Value("${build.name:default}")
+//    private String buildName;
+//
+//    @GetMapping("/build-info")
+//    public String getBuildInfo(){
+//        String buildInfo = "Build Id: " + buildId + " Build Version: " + buildVersion + " Build Name: " + buildName;
+//        return buildInfo;
+//    }
+//}
 
-//    @Value("${build.id}")
-//    @Value("${OS:default}")
-//    If we provide default value and also we enable default profile active ,
-//    so this will show values from application.yml file because i activate default profile
-    @Value("${build.id:default}")
-    private String buildId;
 
-//    @Value("${USERPROFILE:default}")
+@RestController
+@AllArgsConstructor
+public class BuildInforController {
 
-    @Value("${build.version:default}")
-    private String buildVersion;
-
-//    @Value("${JAVA_HOME:default}")
-    @Value("${build.name:default}")
-    private String buildName;
+    private final BuildInfo buildInfo;
 
     @GetMapping("/build-info")
-    public String getBuildInfo(){
-        String buildInfo = "Build Id: " + buildId + " Build Version: " + buildVersion + " Build Name: " + buildName;
-        return buildInfo;
+    public String getBuildInfo() {
+        return "Build Id: " + buildInfo.getId()
+                + " Build Version: " + buildInfo.getVersion()
+                + " Build Name: " + buildInfo.getName();
     }
 }
