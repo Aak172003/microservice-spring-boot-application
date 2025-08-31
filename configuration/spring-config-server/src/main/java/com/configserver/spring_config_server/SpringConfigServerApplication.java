930e7e910f3d7b3af6d8847c4016071d79d7be17
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-08-31 11:59:51 +0530

    mapping-spring-config-server-to-git-repository

--- configuration/spring-config-server/src/main/java/com/configserver/spring_config_server/SpringConfigServerApplication.java
+++ configuration/spring-config-server/src/main/java/com/configserver/spring_config_server/SpringConfigServerApplication.java
@@ -2,11 +2,15 @@ package com.configserver.spring_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+//@EnableConfigServer -> It will enable the feature of spring config server
 @SpringBootApplication
+@EnableConfigServer
 public class SpringConfigServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Start Spring Cloud Configuration Server");
 		SpringApplication.run(SpringConfigServerApplication.class, args);
 	}
 
